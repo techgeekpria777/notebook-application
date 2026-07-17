058d60bc304920254b8df7889020ebcbf8e65ea6
techgeekpria777 <techgeekpria@gmail.com>
2026-07-17 19:08:19 +0530

    deploy dynamodb tables

--- infra/main.tf
+++ infra/main.tf
@@ -11,3 +11,30 @@ provider "aws" {
   region  = "ap-south-1"
   profile = "notebook-application"
 }
+
+resource "aws_dynamodb_table" "token_email_lookup" {
+  name         = "token-email-lookup"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "token"
+
+  attribute {
+    name = "token"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "user_notes" {
+  name         = "user-notes"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "user"
+  range_key    = "create_date"
+
+  attribute {
+    name = "user"
+    type = "S"
+  }
+  attribute {
+    name = "create_date"
+    type = "S"
+  }
+}
