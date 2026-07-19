7e238941b19b4706b310904b34b51a9f021a01a7
techgeekpria777 <techgeekpria@gmail.com>
2026-07-19 19:37:31 +0530

    create lambda function for notebook-handler

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -46,3 +46,12 @@ resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_lambda_function" "notebook_application_lambda" {
+  function_name    = "notebook-application-handler"
+  role             = aws_iam_role.lambda_role.arn
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+}
